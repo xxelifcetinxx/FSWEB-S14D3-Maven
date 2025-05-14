ca5e7f214b3c8c47702b3ab2a4d69a869cad1e60
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:22:28 +0300

    car.java revised, maintest run - 12 tests passed - project done

--- src/main/java/org/example/company/Car.java
+++ src/main/java/org/example/company/Car.java
@@ -1,11 +1,9 @@
 package org.example.company;
 
-import java.util.Objects;
-
 public class Car {
-    private boolean engine;
-    private int cylinders;
     private String name;
+    private int cylinders;
+    private boolean engine;
     private int wheels;
 
     public Car(int cylinders, String name) {
@@ -15,40 +13,26 @@ public class Car {
         this.wheels = 4;
     }
 
-    public int getCylinders() {
-        return cylinders;
+    public String startEngine() {
+        System.out.println(getClass().getSimpleName());
+        return "the car's engine is starting";
+    }
+
+    public String accelerate() {
+        System.out.println(getClass().getSimpleName());
+        return "the car is accelerating";
+    }
+
+    public String brake() {
+        System.out.println(getClass().getSimpleName());
+        return "the car is braking";
     }
 
     public String getName() {
         return name;
     }
 
-    public String startEngine() {
-        return getClass().getSimpleName() + " -> the car's engine is starting";
-    }
-
-    public String accelerate() {
-        return getClass().getSimpleName() + " -> the car is accelerating";
-    }
-
-    public String brake() {
-        return getClass().getSimpleName() + " -> the car is braking";
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "cylinders=" + cylinders +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Car)) return false;
-        Car car = (Car) obj;
-        return cylinders == car.cylinders &&
-                Objects.equals(name, car.name);
+    public Integer getCylinders() {
+        return cylinders;
     }
 }
