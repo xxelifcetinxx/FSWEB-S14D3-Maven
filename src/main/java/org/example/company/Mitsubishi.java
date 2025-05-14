5b2220680964a67b48850e583af333ded628e945
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:04:29 +0300

    Mitsubishi ok

--- src/main/java/org/example/company/Mitsubishi.java
+++ src/main/java/org/example/company/Mitsubishi.java
@@ -1,4 +1,22 @@
 package org.example.company;
 
-public class Mitsubishi {
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> engine starting with turbo";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerating with AWD";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> braking with ABS";
+    }
 }
