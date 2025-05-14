7baccfc795fc6b94c11e0afc210df4010ed97a58
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:06:04 +0300

    CarSkeleton ok

--- src/main/java/org/example/arge/CarSkeleton.java
+++ src/main/java/org/example/arge/CarSkeleton.java
@@ -1,4 +1,34 @@
 package org.example.arge;
 
 public class CarSkeleton {
+    private String name;
+    private String description;
+
+    public CarSkeleton() {}
+
+    public CarSkeleton(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startEngine() {
+        System.out.println(getClass().getSimpleName() + " -> engine starting...");
+    }
+
+    public void drive() {
+        System.out.println(getClass().getSimpleName() + " -> driving...");
+        runEngine();
+    }
+
+    protected void runEngine() {
+        System.out.println(getClass().getSimpleName() + " -> running engine logic...");
+    }
 }
