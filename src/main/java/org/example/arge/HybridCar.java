242ba613eb45f4e4a306ebacaf63b6c3f62c95f1
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:08:38 +0300

    HybridCar ok

--- src/main/java/org/example/arge/HybridCar.java
+++ src/main/java/org/example/arge/HybridCar.java
@@ -1,4 +1,42 @@
 package org.example.arge;
 
-public class HybridCar {
+public class HybridCar extends CarSkeleton {
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders;
+
+    public HybridCar(String name, String description, double avgKmPerLitre, int batterySize, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println(getClass().getSimpleName() + " -> starting hybrid system...");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(getClass().getSimpleName() + " -> driving hybrid: " + avgKmPerLitre + " km/l, battery " + batterySize + "kWh.");
+        runEngine();
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.println(getClass().getSimpleName() + " -> combining electric + gas.");
+    }
 }
