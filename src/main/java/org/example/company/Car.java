7a239a25d0087fec864b784acead80718d65c138
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:04:02 +0300

    car ok

--- src/main/java/org/example/company/Car.java
+++ src/main/java/org/example/company/Car.java
@@ -1,4 +1,54 @@
 package org.example.company;
 
+import java.util.Objects;
+
 public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.engine = true;
+        this.wheels = 4;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> the car's engine is starting";
+    }
+
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> the car is accelerating";
+    }
+
+    public String brake() {
+        return getClass().getSimpleName() + " -> the car is braking";
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "cylinders=" + cylinders +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Car)) return false;
+        Car car = (Car) obj;
+        return cylinders == car.cylinders &&
+                Objects.equals(name, car.name);
+    }
 }
