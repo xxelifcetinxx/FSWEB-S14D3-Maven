a71b77cb49cd42ffbe59c6a47d590d84a8e11acb
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:07:50 +0300

    ElectricCar ok

--- src/main/java/org/example/arge/ElectricCar.java
+++ src/main/java/org/example/arge/ElectricCar.java
@@ -1,4 +1,36 @@
 package org.example.arge;
 
-public class ElectricCar {
+public class ElectricCar extends CarSkeleton {
+    private double avgKmPerCharge;
+    private int batterySize;
+
+    public ElectricCar(String name, String description, double avgKmPerCharge, int batterySize) {
+        super(name, description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    public double getAvgKmPerCharge() {
+        return avgKmPerCharge;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println(getClass().getSimpleName() + " -> electric motor ready.");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(getClass().getSimpleName() + " -> driving with " + batterySize + "kWh battery.");
+        runEngine();
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.println(getClass().getSimpleName() + " -> silent power...");
+    }
 }
