b557bd83823a22deae63a2c88f0891d5a8534e44
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:05:02 +0300

    ford ok

--- src/main/java/org/example/company/Ford.java
+++ src/main/java/org/example/company/Ford.java
@@ -1,4 +1,22 @@
 package org.example.company;
 
-public class Ford {
+public class Ford extends Car {
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> V8 engine roars!";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> smooth acceleration";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> disc brakes applied";
+    }
 }
