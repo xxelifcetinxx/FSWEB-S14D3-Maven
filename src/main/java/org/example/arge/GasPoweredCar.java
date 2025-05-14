45179bb4637ca3349b8c24510fe8897bc96bc47e
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:07:06 +0300

    GasPoweredCar ok

--- src/main/java/org/example/arge/GasPoweredCar.java
+++ src/main/java/org/example/arge/GasPoweredCar.java
@@ -1,4 +1,36 @@
 package org.example.arge;
 
-public class GasPoweredCar {
+public class GasPoweredCar extends CarSkeleton {
+    private double avgKmPerLitre;
+    private int cylinders;
+
+    public GasPoweredCar(String name, String description, double avgKmPerLitre, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println(getClass().getSimpleName() + " -> ignition on: " + getCylinders() + " cylinders.");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(getClass().getSimpleName() + " -> fuel efficiency: " + avgKmPerLitre + " km/l");
+        runEngine();
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.println(getClass().getSimpleName() + " -> burning gasoline...");
+    }
 }
