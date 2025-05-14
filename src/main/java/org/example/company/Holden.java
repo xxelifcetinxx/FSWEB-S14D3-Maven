65ca0bf23c14f5670a7de6470abfeb6ccabdb0e6
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-14 15:05:36 +0300

    Holden ok

--- src/main/java/org/example/company/Holden.java
+++ src/main/java/org/example/company/Holden.java
@@ -1,4 +1,22 @@
 package org.example.company;
 
-public class Holden {
+public class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startup with rumble";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> high torque launch";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> quick stop with ABS";
+    }
 }
